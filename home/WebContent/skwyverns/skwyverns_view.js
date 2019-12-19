7e1297ce6d7ea8fb6d74b249ea584012bd6dfe40
hanyena <gks963@naver.com>
2019-12-19 09:53:47 +0900

    doxygen 작성

--- home/WebContent/skwyverns/skwyverns_view.js
+++ home/WebContent/skwyverns/skwyverns_view.js
@@ -1,4 +1,15 @@
-
+/**
+ * 	@fn			window.onload = function()
+ *	@brief		페이지 로딩 후 실행 시 해당되지 않는 버튼 숨김
+ *	@details
+ *
+ *	@author		한예나
+ *	@date		2019-12-05
+ *
+ *	@param		
+ *  
+ *	@remark		
+ */
 // window.load는 페이지 로딩 후 실행
 window.onload = function() {
 	var action = document.form1.action.value;
@@ -12,6 +23,18 @@ window.onload = function() {
 	}
 } 
 
+/**
+ * 	@fn			function insertcheck()
+ *	@brief		입력 버튼 클릭 시 값이 없을 경우 처리
+ *	@details
+ *
+ *	@author		한예나
+ *	@date		2019-12-05
+ *
+ *	@param		
+ *  
+ *	@remark		
+ */
 function insertcheck() {
 	// post방식
 	document.form1.action.value="insert";
@@ -46,6 +69,18 @@ function deletecheck() {
 		return;
 }
 
+/**
+ * 	@fn			function backcheck()
+ *	@brief		뒤로가기 버튼 시 경고
+ *	@details
+ *
+ *	@author		한예나
+ *	@date		2019-12-05
+ *
+ *	@param		
+ *  
+ *	@remark		
+ */
 function backcheck() {
 	// post방식
 	alert("저장하지 않고 나가시겠습니까");
